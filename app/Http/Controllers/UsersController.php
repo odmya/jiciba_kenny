a578905c320a546603fc9650b00cae20439cc1a8
sbguh <sales@batterylaptops.co.uk>
2017-11-29 02:38:12 +0000

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class UsersController extends Controller
+{
+    //
+    public function __construct()
+      {
+          $this->middleware('auth', [
+              'except' => ['show', 'create', 'store']
+          ]);
+
+          $this->middleware('guest', [
+              'only' => ['create']
+          ]);
+      }
+
+      public function index()
+    {
+      //$this->authorize('view');
+
+      if(Auth::user()->is_admin){
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+      }else{
+        return redirect('login');
+      }
+
+
+    }
+
+
+    public function create()
+      {
+          return view('users.create');
+      }
+
+      public function show(User $user)
+      {
+          $this->authorize('view', $user);
+          return view('users.show', compact('user'));
+      }
+
+      public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
+
+    public function edit(User $user)
+    {
+      $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+      {
+          $this->validate($request, [
+              'name' => 'required|max:50',
+              'password' => 'nullable|confirmed|min:6'
+          ]);
+          $this->authorize('update', $user);
+          $data = [];
+          $data['name'] = $request->name;
+          if ($request->password) {
+              $data['password'] = bcrypt($request->password);
+          }
+          $user->update($data);
+
+          session()->flash('success', '个人资料更新成功！');
+
+          return redirect()->route('users.show', $user->id);
+      }
+
+      public function destroy(User $user)
+   {
+       $user->delete();
+       session()->flash('success', '成功删除用户！');
+       return back();
+   }
+
+
+
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
+    <script src="/js/app.js"></script>
+    
+  </body>
+
+
+</html>
