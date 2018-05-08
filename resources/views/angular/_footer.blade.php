bab83f56211fb9ad964cfdad89a058ce42a2718d
sbguh <sales@batterylaptops.co.uk>
2018-05-08 05:55:54 +0000

    add anglularjs

--- resources/views/angular/_footer.blade.php
+++ resources/views/angular/_footer.blade.php
@@ -2,7 +2,7 @@
   <footer class="footer">
     <nav>
 
-        <ul class="navbar navbar-default navbar-fixed-bottom">
+        <ul class="navbar navbar-default ">
 
             <li><a href="{{ route('about') }}">关于</a></li>
 
