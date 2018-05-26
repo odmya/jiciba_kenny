fc2b3cbab9d0c5bda01df748cae9ad0a392ccd08
sbguh <sales@batterylaptops.co.uk>
2018-05-26 01:06:51 +0000

    add wechat

--- resources/views/sessions/wechatlogin.blade.php
+++ resources/views/sessions/wechatlogin.blade.php
@@ -16,15 +16,18 @@
 wx.config({!! $app->jssdk->buildConfig(array('scanQRCode'), true) !!});
 
 wx.ready(function () {
-  wx.scanQRCode({
-    desc: 'scanQRCode desc',
-    needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-    success: function (res) {
-    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+
+});
+document.querySelector('#scanQRCode').onclick = function() {
+    wx.scanQRCode({
+      desc: 'scanQRCode desc',
+      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+      scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+      success: function (res) {
+      var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+  }
+  });
 }
-});
-});
 
 </script>
 
@@ -33,7 +36,7 @@ wx.ready(function () {
     <div class="container">
       <div class="col-md-offset-1 col-md-10">
 
-        <div id="wechatlogin"></div>
+        <div id="wechatlogin"><button id="scanQRCode">扫码</button></div>
       </div>
     </div>
 
