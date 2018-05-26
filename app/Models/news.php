257de17a7d18c41fa62e491e78d215a9b9510a42
sbguh <sales@batterylaptops.co.uk>
2018-05-26 08:32:01 +0000

    add commit

--- app/Models/news.php
+++ app/Models/news.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class news extends Model
+class News extends Model
 {
 
   protected $fillable = ['title','sub_title','author','category_id','image','description','is_enable','count'];
